3d295d9fc588a781b76acdbcb4633221842cf32e
Omar Awile <omar.awile@gmail.com>
2017-09-19 23:50:10 +0200

    cleanup code and adjust config

--- assets/js/ghex_config.js
+++ assets/js/ghex_config.js
@@ -1,8 +1,8 @@
-   
 
-var ThemeOptions = 
+
+var ThemeOptions =
 {
-	show_blog_title : false, // true or false - default : false
+	show_blog_title : true, // true or false - default : false
 	show_blog_description : true, // true or false - default : true
 
 	background_type : 'cover', // cover or repeat - default : cover and Blog Cover file (extra/alaska_cover.jpg)
@@ -11,28 +11,27 @@ var ThemeOptions =
 	hex_background : '#fff', // default background color for non featured hexagons - default : #fff
 
 	disqus: { // Disqus Settings
-		enabled : true, // true or false 
+		enabled : true, // true or false
 		shortname : 'ghextheme' //Your Disqus shortname
 	},
 
 	staticPages: { // Static Pages Menu
-			enabled : false, // true or false - show static pages 
+			enabled : false, // true or false - show static pages
 			pages :[
 	 		   { title: "Static",  url: 'http://www.marblehouse.es' },
 	 		   { title: "Pages",  url: 'http://www.marblehouse.es' },
 	 		   { title: "Example",  url: 'http://www.marblehouse.es' }
-	 		 
+
 			]
 	},
-		
+
 	socialAccounts: { //Social Icons - SEARCH for icon letter at: http://energee.github.io/social_font_face/
 		enabled : true, // true or false - show social buttons
-		position : 'top', // top or bottom - social buttons position 
+		position : 'top', // top or bottom - social buttons position
 		accounts :[
  		   { name: "twitter", icon: 'T', url: 'https://twitter.com/yourname' },
  		   { name: "facebook", icon: 'F', url: 'https://www.facebook.com/yourname' }
-		] 
-		
+		]
+
 	}
 }
-
